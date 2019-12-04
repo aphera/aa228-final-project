4b330ecf0f95aa35030b8a744456ed13e505648b
George Woskob <georgewoskob@gmail.com>
2019-12-03 21:24:49 -0500

    Given perfect observations on the beat it works perfectly.

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -5,11 +5,13 @@ from timeit import default_timer as timer
 # samples_per_second = 44100
 samples_per_second = 200
 
+transition_rate = (1.0 / 60) / samples_per_second
+
 # position, beats per minute
 x = np.array([0.0, 120.0])
 # Our transition function
 f = np.array(
-    [[1.0, (1.0 / 60) / samples_per_second],
+    [[1.0, transition_rate],
      [0.0, 1.0]]
 )
 f_t = f.transpose()
@@ -21,27 +23,53 @@ p = np.array(
 # Q grows the covariance over time
 # Let's assume after a second we may have drifted by 16bpm
 q = np.array(
-    [[1.0, 8.0],
-     [8.0, 16.0]]
+    [[1.0, 0.0],
+     [0.0, 16.0]]
 ) / samples_per_second
 
-# Our observation function
-h = np.array([[1.2,2],[3,4],[5,6]])
-h_t = h.transpose()
-# Means of observations
-z = np.array([5.5,2,3])
-# The covariance of the observations
-r = np.array([[1.1,2,3],[4,5,6],[7,8,9]])
+r = np.array(
+    [[1.0, 0.0],
+     [0.0, 16.0]]
+) / (samples_per_second)
 
+
+time_delta = r[0,0]
+
+bpm_to_find = 200.0
+
+
+def get_z(i):
+    position_at_100_bpm = i * transition_rate * bpm_to_find
+    return np.array([position_at_100_bpm % 1.0, bpm_to_find])
+
+
+def get_h(s):
+    return np.array(
+        [[1.0, 0.0],
+         [0.0, 1.0]]
+    )
 
 start = timer()
 
-for i in range(0, samples_per_second):
+xs = []
+ts = []
+zs = []
+
+for i in range(0, samples_per_second * 2):
     x = f.dot(x)
     p = f.dot(p).dot(f_t) + q
 
+    x[0] = x[0] % 1
+    z = get_z(i)
+    h = get_h(x)
+    h_t = h.transpose()
+
+    xs.append(x[0])
+    ts.append(x[1])
+    zs.append(z[0])
+
     k_prime = p.dot(h_t).dot(np.linalg.inv(h.dot(p).dot(h_t) + r))
-    x = x + k_prime.dot(z - h.dot(x))
+    x = (x + k_prime.dot(z - h.dot(x)))
     p = p - k_prime.dot(h.dot(p))
 
 end = timer()
@@ -50,7 +78,8 @@ print(f"Time took kalman filter {str(end - start)}")
 print(x)
 print(p)
 
-# plt.figure()
-# plt.plot(a,'b-')
-# plt.plot(b,'r-')
-# plt.show()
+plt.figure()
+# plt.plot(xs,'b-')
+# plt.plot(zs,'r-')
+plt.plot(ts,'r-')
+plt.show()
