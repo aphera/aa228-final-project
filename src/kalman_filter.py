725ab3632c3009954ed5fa8979b99664d00bf467
George Woskob <georgewoskob@gmail.com>
2019-12-04 23:01:46 -0500

    Beautifully finds bpms in midi files.

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -1,97 +1,81 @@
-import random
-
 import numpy as np
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
+from src.midi_reader import get_observations
 
-# samples_per_second = 44100
-samples_per_second = 200
+# tempo at each step
+xs = []
+# pre slide matched observation at each step
+ss = []
+# observation at each step
+zs = []
+# variance at each step
+ps = []
+# if known, actual tempo at each step
+ts = []
 
-# bpm
-x = np.array([[120.0]])
-h = np.array([[1.0]])
-
-# our covariance matrix. After a second, we may stray by 16 bpm
-p = np.array([[16.0]])
-
-# Q grows the covariance over time
-q = 0.01 / samples_per_second
-
-data = {
-    "time_of_previous_beat": 0,
-    "bpm_to_find": 109.0,
-}
 
 observation_base_vector_tuples = np.arange(0.25, 4.25, 0.25)
 
-transition_rate = (1.0 / 60) / samples_per_second
-
-ds = []
 
 def find_z_in_vector_and_compute_r(observation_vector, bpm_estimate):
     idx = (np.abs(observation_vector - bpm_estimate)).argmin()
     z_bpm = observation_vector[idx]
     diff = abs(bpm_estimate - z_bpm)
-    ds.append(diff)
-    return np.array([[z_bpm]]), np.array([[16 + diff * diff]])
+    return np.array([[z_bpm]]), np.array([[1.0 + diff * diff]])
 
 
-def z_activation(sample_in_beat, samples_per_beat):
-    return random.choice([(sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2])), False])
-    # return sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2])
-    # return sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2]) or sample_in_beat == random.choice([0, samples_per_beat / 4])
+def get_z_and_r(observation, bpm_estimate):
+    if observation.seconds_since_last_beat == 0:
+        return None
+    observed_bpm = 60 / observation.seconds_since_last_beat
+    ss.append(observed_bpm)
+    ts.append(observation.actual_bpm)
+    return find_z_in_vector_and_compute_r(observation_base_vector_tuples * observed_bpm, bpm_estimate)
 
 
-def get_z_and_r(t, bpm_estimate):
-    # data["bpm_to_find"] = data["bpm_to_find"] - 0.0001
-    samples_per_beat = int(1 / ((data["bpm_to_find"] / 60) / samples_per_second))
-    sample_in_beat = (t % samples_per_beat)
-    if z_activation(sample_in_beat, samples_per_beat):
-        time_since_last_beat = t - data["time_of_previous_beat"]
-        data["time_of_previous_beat"] = t
-        if time_since_last_beat:
-            relative_bpm = 1 / (time_since_last_beat * transition_rate)
-            return find_z_in_vector_and_compute_r(observation_base_vector_tuples * relative_bpm, bpm_estimate)
+def calculate_beat():
+    # bpm
+    x = np.array([[120.0]])
+    h = np.array([[1.0]])
+    h_t = h.transpose()
+
+    # our covariance matrix
+    p = np.array([[160.0]])
+
+    # Q grows the covariance over time
+    q_per_second = 16.0
+
+    observations = get_observations()
+
+    start = timer()
+    for observation in observations:
+        p = p + q_per_second * observation.seconds_since_last_beat
+        z_and_r = get_z_and_r(observation, h.dot(x)[0, 0])
+        if z_and_r is not None:
+            z, r = z_and_r
+
+            k_prime = p.dot(h_t).dot(np.linalg.inv(h.dot(p).dot(h_t) + r))
+            x = x + k_prime.dot(z - h.dot(x))
+            p = p - k_prime.dot(h.dot(p))
+            zs.append(z[0, 0])
+            xs.append(x[0, 0])
+
+        ps.append(p[0, 0])
+
+    end = timer()
+    print(f"Time took kalman filter {str(end - start)}")
+    print(x[0, 0])
+    print(p[0, 0])
 
 
-xs = []
-zs = []
-ps = []
-
-start = timer()
-
-
-for i in range(0, samples_per_second * 4000):
-    p = p + q
-
-    z_and_r = get_z_and_r(i, h.dot(x)[0, 0])
-    if z_and_r is not None:
-        z, r = z_and_r
-        h_t = h.transpose()
-
-        k_prime = p.dot(h_t).dot(np.linalg.inv(h.dot(p).dot(h_t) + r))
-        x = x + k_prime.dot(z - h.dot(x))
-        p = p - k_prime.dot(h.dot(p))
-        zs.append(z[0, 0])
-        xs.append(x[0, 0])
-
-    ps.append(p[0, 0])
-
-end = timer()
-print(f"Time took kalman filter {str(end - start)}")
-
-print(x[0, 0])
-print(p)
-
+calculate_beat()
 plt.figure()
 plt.plot(xs, 'b-')
-# plt.plot(ds, 'g-')
-# plt.plot(zs, 'r-')
+plt.plot(ts, 'g-')
+plt.plot(zs, 'r-')
 
 # plt.figure()
 # plt.plot(ps[-4000:],'y-')
-
-# plt.figure()
-# plt.plot(ids,'y-')
 
 plt.show()
--- src/midi_reader.py
+++ src/midi_reader.py
@@ -1,12 +1,26 @@
-from mido import MidiFile, MetaMessage, Message
+from mido import MidiFile, tempo2bpm
 
-# midi_file = MidiFile("01prelud.mid")
-midi_file = MidiFile("var8.mid")
-length = 0
-for msg in midi_file:
-    length += msg.time
-    if msg.type == "note_on" and msg.velocity > 0:
-    # if isinstance(msg, Message):
-        print(msg)
 
-print(length)
+class Observation:
+    def __init__(self, seconds_since_last_beat, seconds_since_start, actual_bpm=None):
+        self.seconds_since_last_beat = seconds_since_last_beat
+        self.seconds_since_start = seconds_since_start
+        self.actual_bpm = actual_bpm
+
+
+def get_observations():
+    midi_file = MidiFile("cs1-1pre.mid")
+    # midi_file = MidiFile("vs1-1ada.mid")
+    tempo = 0
+    seconds_since_last_beat = 0
+    seconds_since_start = 0
+    observations = []
+    for msg in midi_file:
+        seconds_since_start += msg.time
+        if msg.type == "set_tempo":
+            tempo = tempo2bpm(msg.tempo)
+        if msg.type == "note_on" and msg.velocity > 0:
+            observations.append(Observation(seconds_since_last_beat, seconds_since_start, tempo))
+            seconds_since_last_beat = 0
+        seconds_since_last_beat += msg.time
+    return observations
