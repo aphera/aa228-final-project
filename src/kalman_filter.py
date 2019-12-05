8aecbbc16df0c9ddc1bd652352757501de64c3a4
George Woskob <georgewoskob@gmail.com>
2019-12-04 20:50:56 -0500

    r is also a function of observation.

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -9,6 +9,7 @@ samples_per_second = 200
 
 # bpm
 x = np.array([[120.0]])
+h = np.array([[1.0]])
 
 # our covariance matrix. After a second, we may stray by 16 bpm
 p = np.array([[16.0]])
@@ -25,26 +26,20 @@ observation_base_vector_tuples = np.arange(0.25, 4.25, 0.25)
 
 transition_rate = (1.0 / 60) / samples_per_second
 
-ids = []
 
-def find_closest_in_vector(observation_vector, bpm_estimate):
+def find_z_in_vector_and_compute_r(observation_vector, bpm_estimate):
     idx = (np.abs(observation_vector - bpm_estimate)).argmin()
-    ids.append(idx)
-    return observation_vector[idx]
-
-
-r = np.array([[16.0]])
-# r = np.array([[0.1]])
-h = np.array([[1.0]])
+    return np.array([[observation_vector[idx]]]), np.array([[16.0]])
 
 
 def z_activation(sample_in_beat, samples_per_beat):
-    # if random.choice([(sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2])), False]):
-    return sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2])
-    # if sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2]) or sample_in_beat == random.choice([0, samples_per_beat / 4]):
+    return random.choice([(sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2])), False])
+    # return sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2])
+    # return sample_in_beat == 0 or sample_in_beat == random.choice([0, samples_per_beat / 2]) or sample_in_beat == random.choice([0, samples_per_beat / 4])
 
-def get_z(t, bpm_estimate):
-    data["bpm_to_find"] = data["bpm_to_find"] - 0.0001
+
+def get_z_and_r(t, bpm_estimate):
+    # data["bpm_to_find"] = data["bpm_to_find"] - 0.0001
     samples_per_beat = int(1 / ((data["bpm_to_find"] / 60) / samples_per_second))
     sample_in_beat = (t % samples_per_beat)
     if z_activation(sample_in_beat, samples_per_beat):
@@ -52,8 +47,8 @@ def get_z(t, bpm_estimate):
         data["time_of_previous_beat"] = t
         if time_since_last_beat:
             relative_bpm = 1 / (time_since_last_beat * transition_rate)
-            closest_tuple = find_closest_in_vector(observation_base_vector_tuples * relative_bpm, bpm_estimate)
-            return np.array([[closest_tuple]])
+            return find_z_in_vector_and_compute_r(observation_base_vector_tuples * relative_bpm, bpm_estimate)
+
 
 xs = []
 zs = []
@@ -65,8 +60,9 @@ start = timer()
 for i in range(0, samples_per_second * 4000):
     p = p + q
 
-    z = get_z(i, h.dot(x)[0, 0])
-    if z is not None:
+    z_and_r = get_z_and_r(i, h.dot(x)[0, 0])
+    if z_and_r is not None:
+        z, r = z_and_r
         h_t = h.transpose()
 
         k_prime = p.dot(h_t).dot(np.linalg.inv(h.dot(p).dot(h_t) + r))
