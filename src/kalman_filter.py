39dc93e1cb65264959c54565d84c33fd81e4421d
George Woskob <georgewoskob@gmail.com>
2019-12-05 20:35:16 -0500

    Adding observation matrix to parameters

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -18,24 +18,17 @@ class ResultMetrics:
         self.ts = []
 
 
-observation_base_vector_tuples = np.arange(0.125, 4.25, 0.125)
-
-
-def find_z_in_vector_and_compute_r(observation_vector, bpm_estimate, result_metrics=None):
-    idx = (np.abs(observation_vector - bpm_estimate)).argmin()
-    z_bpm = observation_vector[idx]
-    if result_metrics:
-        result_metrics.ss.append(idx)
-    return z_bpm, 1.0 + abs(bpm_estimate - z_bpm) * abs(bpm_estimate - z_bpm)
-
-
-def get_z_and_r(observation, bpm_estimate, result_metrics=None):
+def get_z_and_r(observation, b_p, result_metrics=None):
     if observation.seconds_since_last_beat == 0:
         return None
     observed_bpm = 60 / observation.seconds_since_last_beat
+    scaled_observation_vector = b_p.observation_scalar_vector * observed_bpm
+    current_bpm_estimate = b_p.h.dot(b_p.x)[0, 0]
+    idx = (np.abs(scaled_observation_vector - current_bpm_estimate)).argmin()
+    z_bpm = scaled_observation_vector[idx]
     if result_metrics:
-        result_metrics.ts.append(observation.actual_bpm)
-    return find_z_in_vector_and_compute_r(observation_base_vector_tuples * observed_bpm, bpm_estimate, result_metrics)
+        result_metrics.ss.append(idx)
+    return z_bpm, 1.0 + abs(current_bpm_estimate - z_bpm) * abs(current_bpm_estimate - z_bpm)
 
 
 class BeatParameters:
@@ -46,6 +39,7 @@ class BeatParameters:
                  q_per_second=16.0,
                  z=np.array([[0.0]]),
                  r=np.array([[0.0]]),
+                 observation_scalar_vector=np.arange(0.125, 4.25, 0.125)
                  ):
         # bpm
         self.x = x
@@ -59,13 +53,15 @@ class BeatParameters:
         self.z = z
         # observation covariance
         self.r = r
+        self.observation_scalar_vector = observation_scalar_vector
+        self.observation_weight_vector = np.ones(observation_scalar_vector.shape)
 
 
 def calculate_beat(b_p, observations, result_metrics=None):
     start = timer()
     for observation in observations:
         b_p.p = b_p.p + b_p.q_per_second * observation.seconds_since_last_beat
-        z_and_r = get_z_and_r(observation, b_p.h.dot(b_p.x)[0, 0], result_metrics)
+        z_and_r = get_z_and_r(observation, b_p, result_metrics)
         if z_and_r is not None:
             b_p.z[0, 0] = z_and_r[0]
             b_p.r[0, 0] = z_and_r[1]
@@ -74,9 +70,10 @@ def calculate_beat(b_p, observations, result_metrics=None):
             b_p.x = b_p.x + k_prime.dot(b_p.z - b_p.h.dot(b_p.x))
             b_p.p = b_p.p - k_prime.dot(b_p.h.dot(b_p.p))
         if result_metrics:
-            result_metrics.zs.append(b_p.z[0, 0])
             result_metrics.xs.append(b_p.x[0, 0])
+            result_metrics.zs.append(b_p.z[0, 0])
             result_metrics.ps.append(b_p.p[0, 0])
+            result_metrics.ts.append(observation.actual_bpm)
 
     end = timer()
     print(f"Time took kalman filter {str(end - start)}")
@@ -92,31 +89,33 @@ def calculate_error(result_metrics):
     for i, x in enumerate(result_metrics.xs):
         actual_bpm = result_metrics.ts[i]
         # Overloading the meaning of this function but it gives us what we want
-        adjusted_estimate = find_z_in_vector_and_compute_r(error_vector * x, actual_bpm)[0]
+        scaled_error_vector = error_vector * x
+        idx = (np.abs(scaled_error_vector - actual_bpm)).argmin()
+        adjusted_estimate = scaled_error_vector[idx]
         error += np.power(actual_bpm - adjusted_estimate, 2) / samples
     end = timer()
     print(f"Time took calculate error {str(end - start)}")
     return error
 
 
-observations = get_observations()
-result_metrics = ResultMetrics()
-calculate_beat(BeatParameters(), observations, result_metrics)
-total_error = calculate_error(result_metrics)
+os = get_observations()
+rm = ResultMetrics()
+calculate_beat(BeatParameters(), os, rm)
+total_error = calculate_error(rm)
 print(f"Error:\n{total_error}")
 
 
-def plot_results(results_metrics):
+def plot_results(result_metrics):
     plt.figure()
     # tempo at each step
-    # plt.plot(result_metrics.xs, 'b-')
+    plt.plot(result_metrics.xs, 'b-')
     # observation at each step
     # plt.plot(result_metrics.zs, 'r-')
     # if known, actual tempo at each step
-    # plt.plot(result_metrics.ts, 'g-')
+    plt.plot(result_metrics.ts, 'g-')
 
-    unique, counts = np.unique(np.array(result_metrics.ss), return_counts=True)
-    plt.plot(unique, counts)
+    # unique, counts = np.unique(np.array(result_metrics.ss), return_counts=True)
+    # plt.plot(unique, counts)
 
     # plt.figure()
     # variance at each step
@@ -125,4 +124,4 @@ def plot_results(results_metrics):
     plt.show()
 
 
-plot_results(result_metrics)
+plot_results(rm)
